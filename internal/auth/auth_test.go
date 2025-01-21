c1d856b2f09de176143e38065a55673115a10c54
Captum <dan.w00z@gmail.com>
2025-01-20 19:46:26 -0500

    Adding failing UT's to test GitHub that it catches failures.

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,49 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	type test struct {
+		name     string
+		headers  map[string][]string
+		expected string
+		err      error
+	}
+
+	tests := []test{
+		{
+			name:     "no auth header",
+			headers:  map[string][]string{},
+			expected: "",
+			err:      ErrNoAuthHeaderIncluded,
+		},
+		{
+			name:     "malformed auth header",
+			headers:  map[string][]string{"Authorization": {""}},
+			expected: "",
+			err:      errors.New("malformed authorization header"),
+		},
+		{
+			name:     "valid auth header",
+			headers:  map[string][]string{"Authorization": {"ApiKey "}},
+			expected: "",
+			err:      nil,
+		}}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			headers := http.Header(tc.headers)
+			apiKey, err := GetAPIKey(headers)
+			if apiKey != tc.expected {
+				t.Errorf("expected %s, got %s", tc.expected, apiKey)
+			}
+			if err != nil && err.Error() != tc.err.Error() {
+				t.Errorf("expected %v, got %v", tc.err, err)
+			}
+		})
+	}
+}
